f6c7e84a9e45a99b50a337c3ed64e8b41f739a4e
岡部 蒼太 <tokabe333@gmail.com>
2019-12-18 18:06:03 +0900

    add : HTML TS JS FIles

---
+++ 006_TypeAnotation.js
@@ -0,0 +1,19 @@
+// プリミティブ型
+var Name = "Steve";
+var heightInCentimeters = 175.3;
+var IsActive = true;
+// 配列型
+var Names = ["Wai", "NGO", "Sasuke"];
+//関数アノテーション，パラメータ型，戻り値型
+var SayHello;
+// SayHelloの実装
+SayHello = function (name) {
+    return "Hello " + name;
+}; //End_Function
+// オブジェクト型アノテーション
+var Person;
+// パーソンオブジェクト実装
+Person = {
+    Name: "Bebebe",
+    HeightInCentimeters: 175.3
+};
---
+++ 006_TypeAnotation.ts
@@ -0,0 +1,24 @@
+// プリミティブ型
+var Name: string = "Steve";
+var heightInCentimeters: number = 175.3;
+var IsActive: Boolean = true;
+
+// 配列型
+var Names: string[] = ["Wai", "NGO", "Sasuke"];
+
+//関数アノテーション，パラメータ型，戻り値型
+var SayHello: (name: string) => string;
+
+// SayHelloの実装
+SayHello = function (name: string){
+    return "Hello " + name;
+} //End_Function
+
+// オブジェクト型アノテーション
+var Person: {Name: string; HeightInCentimeters: number};
+
+// パーソンオブジェクト実装
+Person = {
+    Name: "Bebebe",
+    HeightInCentimeters: 175.3,
+};
