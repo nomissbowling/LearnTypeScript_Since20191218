657107d1b2f097d2cfd300ebd14ae5e58a28c903
tokabe333 <tokabe333@gmail.com>
2019-12-18 23:04:48 +0900

    add : Class

--- 032_Class.ts
+++ 032_Class.ts
@@ -1,3 +1,32 @@
 class Song{
-    
-}
+    constructor(private Artist: string, private Title: string) {
+    } //End_Constructor
+
+    public Play(){
+        console.log("Playing " + this.Title + " by " + this.Artist);
+    } //End_Method
+} //End_Class
+
+class JukeBox {
+    constructor(private Songs: Song[]){}
+
+    public Play(){
+        var song = this.GetRandomSong();
+        song.Play();
+    }
+
+    private GetRandomSong(){
+        var songCount = this.Songs.length;
+        var songIndex = Math.floor(Math.random() * songCount);
+        return this.Songs[songIndex];
+    } //End_method
+} //End_Class
+
+var songs = [
+    new Song("unti", "magephone"),
+    new Song("Unpi", "pipip"),
+    new Song("ngngng", "ngogogo"),
+]
+
+var jukeBox = new JukeBox(songs);
+jukeBox.Play();
--- JS/032_Class.js
+++ JS/032_Class.js
@@ -1,4 +1,32 @@
 "use strict";
 class Song {
-}
+    constructor(Artist, Title) {
+        this.Artist = Artist;
+        this.Title = Title;
+    } //End_Constructor
+    Play() {
+        console.log("Playing " + this.Title + " by " + this.Artist);
+    } //End_Method
+} //End_Class
+class JukeBox {
+    constructor(Songs) {
+        this.Songs = Songs;
+    }
+    Play() {
+        var song = this.GetRandomSong();
+        song.Play();
+    }
+    GetRandomSong() {
+        var songCount = this.Songs.length;
+        var songIndex = Math.floor(Math.random() * songCount);
+        return this.Songs[songIndex];
+    } //End_method
+} //End_Class
+var songs = [
+    new Song("unti", "magephone"),
+    new Song("Unpi", "pipip"),
+    new Song("ngngng", "ngogogo"),
+];
+var jukeBox = new JukeBox(songs);
+jukeBox.Play();
 //# sourceMappingURL=032_Class.js.map
