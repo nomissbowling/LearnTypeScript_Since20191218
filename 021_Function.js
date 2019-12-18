061242b2964cfe6b64f9e097b50057c9f20fdd4e
tokabe333 <tokabe333@gmail.com>
2019-12-18 22:37:52 +0900

    update : 色んなタイプの関数定義

--- 021_Function.js
+++ 021_Function.js
@@ -4,6 +4,7 @@ function CalcAverage(a, b, c) {
     return "The Average is " + (total / 3);
 } //End_Function
 console.log(CalcAverage(1919, 334, 810));
+// オプションパラメータ
 function GetAverage(a, b, c) {
     let total = a + b;
     let count = 2;
@@ -15,4 +16,33 @@ function GetAverage(a, b, c) {
 } //End_Function
 console.log(GetAverage(1919, 334));
 console.log(GetAverage(1919, 810, 334));
+// デフォルトパラメータ
+function Concatenate(items, separater = ",", beginAt = 0, endAt = items.length) {
+    var result = "";
+    for (var i = beginAt; i < endAt; ++i) {
+        result += items[i];
+        if (i < (endAt - 1)) {
+            result += separater;
+        }
+    } //End_For
+    return result;
+} //End_Function
+var items = ["AA", "BBB", "CCCC"];
+var result = Concatenate(items);
+console.log(result);
+var result = Concatenate(items, "_", 0, 2);
+console.log(result);
+// レストパラメータ
+function RestAverage(...a) {
+    var total = 0;
+    var count = 0;
+    for (var i = 0; i < a.length; ++i) {
+        total += a[i];
+        count += 1;
+    } //End_For
+    return "The average is " + (total / count);
+} //End_Function
+console.log(RestAverage(2, 4, 6, 8, 10));
+console.log(RestAverage(114, 514, 1919, 810));
+// アロー関数
 //# sourceMappingURL=021_Function.js.map
--- 021_Function.ts
+++ 021_Function.ts
@@ -4,6 +4,7 @@ function CalcAverage(a:number, b:number, c:number): string{
 } //End_Function
 console.log(CalcAverage(1919, 334, 810));
 
+// オプションパラメータ
 function GetAverage(a:number, b:number, c?:number): string{
     let total = a + b;
     let count = 2;
@@ -15,3 +16,31 @@ function GetAverage(a:number, b:number, c?:number): string{
 } //End_Function
 console.log(GetAverage(1919,334));
 console.log(GetAverage(1919,810,334))
+
+// デフォルトパラメータ
+function Concatenate(items:string[], separater:string = ",", beginAt:number = 0, endAt:number = items.length): string{
+    var result = "";
+    for(var i = beginAt; i < endAt; ++i){
+        result += items[i];
+        if(i < (endAt - 1)){ result += separater;}
+    } //End_For
+    return result;
+} //End_Function
+var items = ["AA","BBB","CCCC"];
+var result = Concatenate(items);
+console.log(result);
+var result = Concatenate(items, "_", 0, 2);
+console.log(result);
+
+// レストパラメータ
+function RestAverage(...a:number[]): string{
+    var total = 0;
+    var count = 0;
+    for(var i = 0; i < a.length; ++i){
+        total += a[i];
+        count += 1;
+    } //End_For
+    return "The average is " + (total / count);
+} //End_Function
+console.log(RestAverage(2,4,6,8,10));
+console.log(RestAverage(114,514,1919,810));
